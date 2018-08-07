07dc8c8a8269ad112605896573e2c82bd8806519
胡乐天 <786909217@qq.com>
2018-08-07 17:31:02 +0800

    add

--- src/main/java/com/elex/oa/service/eqptService/InventoryService.java
+++ src/main/java/com/elex/oa/service/eqptService/InventoryService.java
@@ -18,14 +18,17 @@ public interface InventoryService {
     // 插入盘点信息
     void insertInfo(HttpServletRequest request) throws ParseException;
 
+    // 暂存盘点信息
+    void saveInfo(HttpServletRequest request)throws ParseException;
+
     // 删除盘点信息
     void deleteInfo(HttpServletRequest request);
 
     // 更改盘点信息
-    void changeInfo(HttpServletRequest request);
+    void changeInfo(HttpServletRequest request) throws ParseException;
 
     // 确定盘点单号
-    List showInvId(HttpServletRequest request);
+    List showInvId(HttpServletRequest request) ;
 
     // 查询盘点的仓库内物料情况(选物料)
     List<Material> chooseMat(HttpServletRequest request);
@@ -35,5 +38,14 @@ public interface InventoryService {
 
     // 获取仓库ID
     List<Repository> ReptList();
+
+    // 判断是否为草稿
+    String checkDraft(HttpServletRequest request);
+
+    // 删除草稿
+    void deleteDraft(HttpServletRequest request);
+
+    // 打开草稿
+    List<Repository> openDraft(HttpServletRequest request);
 
 }
