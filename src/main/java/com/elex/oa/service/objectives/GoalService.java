5899458c4c78b88bdf8d036857c14a9edcad9d86
gaoxiaofei <630851511@qq.com>
2018-08-31 17:47:16 +0800

    看板部分

--- src/main/java/com/elex/oa/service/objectives/GoalService.java
+++ src/main/java/com/elex/oa/service/objectives/GoalService.java
@@ -3,6 +3,7 @@ package com.elex.oa.service.objectives;
 import com.elex.oa.entity.objectives.Goal;
 import com.elex.oa.entity.objectives.Goal2;
 
+import java.util.List;
 import java.util.Map;
 
 public interface GoalService {
@@ -16,4 +17,10 @@ public interface GoalService {
     Goal2 obtainGrossUnit(String unit);
     //获取某单位的销售毛利详情
     Goal obtainPatentUnit(String unit);
+    //获取公司相应数据(手机端)
+    List<Map<String,String>> obtainCompany(String unit);
+    //获取公司销售收入或税后净利相应数据（手机端）
+    List<Map<String,String>> obtainVarious(String unit, String various);
+    //获取单位对应的销售收入或税后净利相应数据详情（手机端）
+    Map<String,String> obtainDetails(String unit, String various);
 }
