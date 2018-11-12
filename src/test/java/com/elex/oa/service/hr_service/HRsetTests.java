54719634ac51f17930906b2c54e7b35bea009e77
shiyun <17749536699@163.com>
2018-11-12 11:20:57 +0800

    HRsetTests添加测试代码（Service层）： 1.删除多条 2.判断是否存在 Signed-off-by: shiyun <17749536699@163.com>

--- src/test/java/com/elex/oa/service/hr_service/HRsetTests.java
+++ src/test/java/com/elex/oa/service/hr_service/HRsetTests.java
@@ -1,9 +1,7 @@
 package com.elex.oa.service.hr_service;
 
-import com.elex.oa.dao.hr.IHRsetDao;
 import com.elex.oa.entity.hr_entity.HRset;
 import com.github.pagehelper.PageInfo;
-import org.assertj.core.api.AbstractBooleanAssert;
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
@@ -14,8 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Description: HRsetService测试
@@ -31,59 +31,78 @@ public class HRsetTests {
     private IHRsetService ihRsetService;
 
     @Test
-    public void test1_addOne(){
+    public void test1_addOne() {
         ihRsetService.addOne(new HRset("race", "藏族"));
         List<HRset> hRsetList = ihRsetService.queryByConditions(new HRset("race", "藏族"));
-        Assert.assertNotNull("test1_addOne hRsetList is null",hRsetList);
-        Assert.assertEquals("hRsetList.size is wrong",1,hRsetList.size());
+        Assert.assertNotNull("test1_addOne hRsetList is null", hRsetList);
+        Assert.assertEquals("hRsetList.size is wrong", 1, hRsetList.size());
     }
 
     @Test
-    public void test2_queryAll(){
+    public void test2_queryAll() {
         List<HRset> hRsetList = ihRsetService.queryAll();
-        Assert.assertNotNull("test2_queryAll hRsetList is null",hRsetList);
-        Assert.assertEquals("test2_queryAll hRsetList.size is wrong",3,hRsetList.size());
+        Assert.assertNotNull("test2_queryAll hRsetList is null", hRsetList);
+        Assert.assertEquals("test2_queryAll hRsetList.size is wrong", 3, hRsetList.size());
     }
 
     @Test
-    public void test3_queryByConditions(){
+    public void test3_queryByConditions() {
         List<HRset> hRsetList = ihRsetService.queryByConditions(new HRset("race"));
-        Assert.assertNotNull("test3_queryByConditions hRsetList is null",hRsetList);
-        Assert.assertEquals("test3_queryByConditions hRsetList.size is wrong",3,hRsetList.size());
+        Assert.assertNotNull("test3_queryByConditions hRsetList is null", hRsetList);
+        Assert.assertEquals("test3_queryByConditions hRsetList.size is wrong", 3, hRsetList.size());
     }
 
     @Test
-    public void test4_queryById(){
+    public void test4_queryById() {
         HRset hRset = ihRsetService.queryById(1);
-        Assert.assertNotNull("test4_queryById hRset is null",hRset);
+        Assert.assertNotNull("test4_queryById hRset is null", hRset);
     }
 
     @Test
-    public void test5_queryByParam(){
+    public void test5_queryByParam() {
         HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("pageNum",1);
-        paramMap.put("pageSize",5);
-        paramMap.put("entity",new HRset("race"));
+        paramMap.put("pageNum", 1);
+        paramMap.put("pageSize", 5);
+        paramMap.put("entity", new HRset("race"));
         PageInfo pageInfo = ihRsetService.queryByParam(paramMap);
-        Assert.assertEquals("test5_queryByParam pageInfo.total is wrong",3,pageInfo.getTotal());
+        Assert.assertEquals("test5_queryByParam pageInfo.total is wrong", 3, pageInfo.getTotal());
     }
 
     @Test
-    public void test6_removeOne(){
+    public void test6_removeOne() {
         Boolean res = ihRsetService.removeOne(1);
-        Assert.assertTrue("test6_removeOne delete fail",res);
+        Assert.assertTrue("test6_removeOne delete fail", res);
     }
 
     @Test
-    public void test7_modifyOne(){
+    public void test7_modifyOne() {
         HRset hRset = ihRsetService.modifyOne(new HRset(2, "汉族"));
-        Assert.assertNotNull("test7_modifyOne hRset is null",hRset);
-        Assert.assertEquals("test7_modifyOne hRset.datavalue","汉族",hRset.getDatavalue());
+        Assert.assertNotNull("test7_modifyOne hRset is null", hRset);
+        Assert.assertEquals("test7_modifyOne hRset.datavalue", "汉族", hRset.getDatavalue());
     }
 
     @Test
-    public void test8_modifyHRset(){
+    public void test8_modifyHRset() {
         Boolean aBoolean = ihRsetService.modifyHRset(new HRset(2, "藏族"));
         Assertions.assertThat(aBoolean).isTrue();
+    }
+
+    @Test
+    public void test9_removeMultiple() {
+        List<Integer> ids = new ArrayList<>();
+        ids.add(1);
+        ids.add(2);
+        Map<Integer, String> map = ihRsetService.removeMultiple(ids);
+        Assertions.assertThat(map.size()).isEqualTo(2);
+        Assertions.assertThat(map.get(1)).isEqualTo("删除成功！");
+        Assertions.assertThat(map.get(2)).isEqualTo("删除成功！");
+    }
+
+    @Test
+    public void test10_queryValidateHRset(){
+        Boolean rset1 = ihRsetService.queryValidateHRset(new HRset("race", "汉族"));
+        Assertions.assertThat(rset1).isTrue();
+        Boolean rset2 = ihRsetService.queryValidateHRset(new HRset("race", "蒙古族"));
+        Assertions.assertThat(rset2).isTrue();
     }
 }    
