49f64dbb48f3d4bb27112caa239632e9dc7677d4
zhaohonggang <zhaohonggang@elextec.com>
2019-04-18 17:19:05 +0800

    修复项目立项流程审批结束后找不到项目的问题
    Change-Id: I4d360fd4a0f4bbfc984bd7ca147ed3568fb65817

--- src/main/java/com/elex/oa/schedule/ProjectSchedule.java
+++ src/main/java/com/elex/oa/schedule/ProjectSchedule.java
@@ -1,6 +1,4 @@
 package com.elex.oa.schedule;
-
-import com.elex.oa.service.project.ProjectBoardService;
 import com.elex.oa.service.project.ProjectInforService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,16 +10,13 @@ public class ProjectSchedule {
     @Resource
     private ProjectInforService projectInforService;
 
-    @Resource
-    private ProjectBoardService projectBoardService;
-
     @Scheduled(cron = "0 0/3 * * * ?")
     public void addInfor() {
-        /*projectInforService.addInfor();*/
+        projectInforService.addInfor();
     }
 
-    @Scheduled(cron = "0 45 23 * * ?")
+ /*   @Scheduled(cron = "0 45 23 * * ?")
     public void updateStaff() {
-       /*projectBoardService.informationUpdate();*/
-    }
+      projectBoardService.informationUpdate();
+    }*/
 }
